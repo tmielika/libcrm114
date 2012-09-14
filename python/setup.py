cc4ca994f7ca0d6d003e0353c047fe8cc4b3805f
Taneli Mielikainen <taneli.mielikainen@nokia.com>
2012-09-13 23:10:05 -0700

    fixed include paths of libaries

--- python/setup.py
+++ python/setup.py
@@ -2,9 +2,10 @@ import sys, os
 from distutils.core import setup, Extension
 
 script_dir = os.path.dirname(os.path.realpath( __file__ ))
-top_dir = os.path.dirname(script_dir)
+#top_dir = os.path.dirname(script_dir)
+top_dir = "/usr/local"
 inc_dir = os.path.join(top_dir, "include")
-lib_dir = os.path.join(top_dir, "lib")
+lib_dir = os.path.join(top_dir, "lib/libcrm114")
 
 pycrm114_module = Extension('pycrm114',
                             sources = ['pycrm114_module.c'],
@@ -15,7 +16,7 @@ pycrm114_module = Extension('pycrm114',
                             extra_compile_args=['-Wall', '-g'])
 
 setup(name = 'pycrm114',
-      version = '0.1.0',
+      version = '0.1.1',
       description = 'Python interface to libcrm114',
       author = 'Prashanth Mundkur',
       author_email = 'prashanth.mundkur at gmail.com',
